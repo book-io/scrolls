d5a388ee1535ac828fd6e2437a909b1dcdb4ed98
Christopher Valerio <christopher@valerio.guru>
2023-09-05 21:30:33 -0600

    updating flake

--- flake.nix
+++ flake.nix
@@ -1,24 +1,43 @@
 {
   inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
     crane.url = "github:ipetkov/crane";
-    utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    self,
-    crane,
-    utils,
-    ...
-  }: let
+  outputs = { self, nixpkgs, utils, rust-overlay, crane, ... }:
+  let
     supportedSystems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux"];
+    overlays = [
+        (import rust-overlay) ];
   in
-    utils.lib.eachSystem supportedSystems
-    (
-      system: {
+    utils.lib.eachSystem supportedSystems ( system: {
+        pkgs = import nixpkgs { inherit system overlays; };
+        rustVersion = pkgs.rust-bin.stable."1.70.0".default;
+        rustPlatform = pkgs.makeRustPlatform {
+          cargo = rustVersion;
+          rustc = rustVersion;
+        };
+
         packages.scrolls = crane.lib.${system}.buildPackage {
           src = self;
         };
         packages.default = self.packages.${system}.scrolls;
+        devShell = pkgs.mkShell {
+          buildInputs =
+            [ (rustVersion.override { extensions = [ "rust-src" "rust-analyzer" ]; }) ];
+            packages = [
+                pkgs.cargo-edit
+                pkgs.just
+                pkgs.systemfd
+                pkgs.cargo-watch
+                pkgs.bacon
+                pkgs.openssl
+            ];
+        };
       }
     );
 }
+
+
